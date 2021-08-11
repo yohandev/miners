6dd9335b6c88b9587278bf0947668d2ebaa551f0
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-08-11 01:23:29 -0700

    Added window open-close logic

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,25 @@
+use winit::event_loop::{ ControlFlow, EventLoop };
+use winit::window::WindowBuilder;
+use winit::event::*;
+
 fn main()
 {
-    
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, _, control_flow|
+    {
+        match event
+        {
+            Event::WindowEvent { ref event, window_id }
+                if window_id == window.id()
+                && matches!(event, WindowEvent::CloseRequested) =>
+            {
+                *control_flow = ControlFlow::Exit
+            },
+            _ => { }
+        }
+    })
 }
